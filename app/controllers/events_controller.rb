cd9a45285ef9d5a0ecab837a8c6646d8c5e1e6c8
Ammar Alakkad <am.alakkad@gmail.com>
2023-08-23 14:48:17 +0300

    Dumping changes

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,61 @@
 class EventsController < ApplicationController
-  def index; end
+  before_action :set_event, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
+  # GET /events or /events.json
+  def index
+    @events = Event.all
+  end
+
+  # GET /events/1 or /events/1.json
+  def show
+  end
+
+  # GET /events/new
+  def new
+    @event = Event.new
+  end
+
+  # GET /events/1/edit
+  def edit
+  end
+
+  # POST /events or /events.json
+  def create
+    @event = current_user.events.build(event_params)
+
+    if @event.save
+      redirect_to event_url(@event), notice: "Event was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /events/1 or /events/1.json
+  def update
+    if @event.update(event_params)
+      redirect_to event_url(@event), notice: "Event was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /events/1 or /events/1.json
+  def destroy
+    @event.destroy
+
+    redirect_to events_url, notice: "Event was successfully destroyed."
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def event_params
+    params.require(:event).permit(:title, :date)
+  end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
+  validates :title, presence: true
+  validates :date, presence: true
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
 end
