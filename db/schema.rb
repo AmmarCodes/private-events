19030ed014d37925f4f2a9cd9024e038c508e654
Ammar Alakkad <am.alakkad@gmail.com>
2023-08-29 14:11:10 +0300

    Add name to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_29_082119) do
+ActiveRecord::Schema.define(version: 2023_08_29_110352) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2023_08_29_082119) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
